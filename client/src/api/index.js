791f0c7c1dc96051b1aa5b4a2ca88066e8e33492
taras ostasha <tonyjoss1990@gmail.com>
2024-06-07 12:07:14 -0400

    add product fetch data

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({ baseURL: 'http://localhost:5000/api' });
 
-export const getOrder = id => {console.log(id); axiosInstance.get(`/orders/${id}`)};
+export const getOrder = id => axiosInstance.get(`/orders/${id}`);
 
-export const saveOrder = data => { console.log(data); return axiosInstance.post('/orders', data)} ;
+export const saveOrder = data => axiosInstance.post('/orders', data);
+
+export const getProductById = id => axiosInstance.get(`/products${id}`);
