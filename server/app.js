064ea8acf7c5a545a17442c25fa05d1602aa2b7e
taras ostasha <tonyjoss1990@gmail.com>
2024-06-12 15:37:32 -0400

    add docker; render react app with express

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = require('./routes');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 const corsOPtions = {
     origin: '*'
@@ -12,5 +13,13 @@ app.use(cors(corsOPtions));
 app.use(express.json());
 
 app.use('/api', router);
+
+// // Serve static files from the build folder
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// For all other requests, send the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 module.exports = app;
